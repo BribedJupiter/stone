ad38df5fe262681b1d73963129f940ad0e69f5cb
Jack Bauer <jgb1017@gmail.com>
2026-02-03 09:58:36 -0600

    Initial WebGL example is functional

--- Stone/app/index.tsx
+++ Stone/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { GLView, ExpoWebGLRenderingContext } from 'expo-gl';
 
 export default function Index() {
   return (
@@ -9,7 +10,59 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <GLView style={{width: 300, height: 300}} onContextCreate={onContextCreate} />
     </View>
   );
+}
+
+function onContextCreate(gl: ExpoWebGLRenderingContext) {
+  // See expo documentation here: https://docs.expo.dev/versions/latest/sdk/gl-view/#usage
+  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+  gl.clearColor(1, 0, 0, 1);
+
+  // Create vertex shader (shape & position)
+  const vert: WebGLShader | null = gl.createShader(gl.VERTEX_SHADER);
+  if (vert === null) {
+    console.log("Error creating vertex shader.");
+    return;
+  } 
+  gl.shaderSource(
+    vert,
+    `
+    void main(void) {
+      gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+      gl_PointSize = 150.0;
+    }
+  `
+  );
+  gl.compileShader(vert);
+
+  // Create fragment shader (color)
+  const frag: WebGLShader | null = gl.createShader(gl.FRAGMENT_SHADER);
+  if (frag === null) {
+    console.log("Error creating fragment shader.");
+    return;
+  } 
+  gl.shaderSource(
+    frag,
+    `
+    void main(void) {
+      gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
+  `
+  );
+  gl.compileShader(frag);
+
+  // Link together into a program
+  const program = gl.createProgram();
+  gl.attachShader(program, vert);
+  gl.attachShader(program, frag);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.POINTS, 0, 1);
+
+  gl.flush();
+  gl.endFrameEXP();
 }
